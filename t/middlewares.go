379dc8e533f3841dadb3307691c48ca3318095a0
Larry Darrelc <larrydarrelc@gmail.com>
2014-02-19 19:51:43 +0800

    Add error message to `LoginRequired`.

--- t/middlewares.go
+++ t/middlewares.go
@@ -4,30 +4,35 @@ import (
     "strconv"
     "net/http"
     "github.com/codegangsta/martini"
+    "github.com/codegangsta/martini-contrib/render"
     "github.com/astaxie/beego/orm"
 )
 
-func LoginRequired(resp http.ResponseWriter, req *http.Request, c martini.Context) {
+func LoginRequired(resp http.ResponseWriter, req *http.Request, c martini.Context, r render.Render) {
     banAccess := func() {
-        resp.WriteHeader(http.StatusUnauthorized)
+        r.JSON(http.StatusUnauthorized, Error("Login required."))
     }
 
     id, err := strconv.Atoi(req.Header.Get("X-ID"))
     if err != nil {
         banAccess()
+        return
     }
     token := req.Header.Get("X-TOKEN")
     user := User{Id: id}
     ok, err := user.CheckLogin(token)
     if err != nil {
         banAccess()
+        return
     }
     if !ok {
         banAccess()
+        return
     }
     err = orm.NewOrm().Read(&user)
     if err != nil {
         banAccess()
+        return
     }
     c.Map(&user)
 }
