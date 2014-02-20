3be95e40b1db0ba184e869ab0e4b6a56905915a7
Larry Darrelc <larrydarrelc@gmail.com>
2014-02-20 10:21:18 +0800

    Add get all tweets view.

--- t/t_view.go
+++ t/t_view.go
@@ -1,7 +1,10 @@
 package t
 
 import (
+    "log"
+    "net/http"
     "github.com/codegangsta/martini"
+    "github.com/codegangsta/martini-contrib/render"
 )
 
 // Routes:
@@ -15,7 +18,7 @@ import (
 //  /t/:id/comment  GET     Get a status' comments
 //  /t/:id/comment  POST    Create a comment for status `:id`
 func TRoute(m *martini.ClassicMartini) {
-    m.Get("/t", Nop)
+    m.Get("/t", getTimeline)
     m.Post("/t", Nop)
 
     m.Get("/t/:id", Nop)
@@ -25,4 +28,16 @@ func TRoute(m *martini.ClassicMartini) {
 
     m.Get("/t/:id/comment", Nop)
     m.Post("/t/:id/comment", Nop)
+}
+
+func getTimeline(r render.Render) {
+    var tweet Tweet
+
+    tweets, err := tweet.All()
+    if err != nil {
+        log.Print("Read all tweets failed.", err)
+        r.JSON(http.StatusForbidden, Error("Read timeline failed."))
+        return
+    }
+    r.JSON(http.StatusOK, tweets)
 }
