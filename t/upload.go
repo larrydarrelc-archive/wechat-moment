dbd096cf08e08e5f41febb2abba58854be515955
Larry Darrelc <larrydarrelc@gmail.com>
2014-02-22 15:35:21 +0800

    Change upload service's api.
    Provide access path & store path.

--- t/upload.go
+++ t/upload.go
@@ -14,40 +14,52 @@ import (
 type Uploader struct {
     // Store destination.
     dest string
+    // Access path prefix.
+    path string
     // Stored file name prefix.
     prefix string
 }
 
 // Map a uploader service into the Martini handler chain.
-func UploadProvider(dest, prefix string) martini.Handler {
-    uploader := NewUploader(dest, prefix)
+func UploadProvider(dest, path, prefix string) martini.Handler {
+    uploader := NewUploader(dest, path, prefix)
     return func(c martini.Context) {
         c.Map(uploader)
     }
 }
 
-func NewUploader(dest, prefix string) (*Uploader) {
-    return &Uploader{dest, prefix}
+func NewUploader(dest, path, prefix string) (*Uploader) {
+    return &Uploader{dest, path, prefix}
 }
 
 // Store a uploaded file.
-func (u *Uploader) Store(upload multipart.File) (error) {
-    dest, err := os.Create(u.NewPath())
+func (u *Uploader) Store(upload multipart.File) (string, error) {
+    name := u.newName()
+    dest, err := os.Create(u.newStorePath(name))
     if err != nil {
-        return err
+        return "", err
     }
     defer dest.Close()
 
     if _, err = io.Copy(dest, upload); err != nil {
-        return err
+        return "", err
     }
-    return nil
+    return u.newAccessPath(name), nil
 }
 
 // Generate a new file path.
-func (u *Uploader) NewPath() (string) {
+func (u *Uploader) newStorePath(name string) (string) {
     // FIXME magic path delimiter
-    return fmt.Sprintf("%s/%s%s", u.dest, u.prefix, generateName())
+    return fmt.Sprintf("%s/%s", u.dest, name)
+}
+
+func (u *Uploader) newAccessPath(name string) (string) {
+    return fmt.Sprintf("%s/%s", u.path, name)
+}
+
+// Generate a new file name.
+func (u *Uploader) newName() (string) {
+    return fmt.Sprintf("%s%s", u.prefix, generateName())
 }
 
 // Simply generate a random name.
